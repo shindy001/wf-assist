7b3509ca4b2e215972179a2b5355994ba0b6a3be
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-16 18:53:58 +0100

    feat: register custom nodes to SvelteFlow

--- src/landing/Landing.svelte
+++ src/landing/Landing.svelte
@@ -5,12 +5,18 @@
         Background,
         MiniMap,
     } from '@xyflow/svelte';
-
     import '@xyflow/svelte/dist/style.css';
+    import {FlowNodeType} from "../components/FlowNodeType";
+    import UrlActionNode from "../components/UrlActionNode.svelte";
+
+    const additionalFlowNodes = {
+        [FlowNodeType.UrlAction]: UrlActionNode,
+    };
 
     let nodes = $state.raw([
         {
             id: '1',
+            type: FlowNodeType.UrlAction,
             data: { label: 'Hello' },
             position: { x: 0, y: 0 },
         },
@@ -31,7 +37,7 @@
 </script>
 
 <div class="w-full h-full">
-    <SvelteFlow bind:nodes bind:edges fitView>
+    <SvelteFlow bind:nodes nodeTypes={additionalFlowNodes} bind:edges fitView>
         <Controls />
         <Background />
         <MiniMap />
