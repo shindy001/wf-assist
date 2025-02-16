fec5ee99bcd86ac2b3b6beb2a337adb8c8d360cf
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-16 19:41:32 +0100

    refactor: spread all props to UrlActionNode, add more default nodes to Landing

--- src/components/UrlActionNode.svelte
+++ src/components/UrlActionNode.svelte
@@ -1,13 +1,14 @@
 ﻿<script lang="ts">
     import { Handle, Position, type NodeProps} from "@xyflow/svelte";
 
+    let { data }: NodeProps = $props();
 </script>
 
 <div>
     <div>
         This is and UrlAction Node
     </div>
-    <Handle type="target" position={Position.Left} />
-    <Handle type="source" position={Position.Right} />
+    <Handle type="target" position={Position.Left} {...data} />
+    <Handle type="source" position={Position.Right} {...data} />
 </div>
 
--- src/landing/Landing.svelte
+++ src/landing/Landing.svelte
@@ -1,10 +1,5 @@
 ﻿<script lang="ts">
-    import {
-        SvelteFlow,
-        Controls,
-        Background,
-        MiniMap,
-    } from '@xyflow/svelte';
+    import {Background, Controls, type Edge, MiniMap, type Node, Position, SvelteFlow,} from '@xyflow/svelte';
     import '@xyflow/svelte/dist/style.css';
     import {FlowNodeType} from "../components/FlowNodeType";
     import UrlActionNode from "../components/UrlActionNode.svelte";
@@ -13,31 +8,69 @@
         [FlowNodeType.UrlAction]: UrlActionNode,
     };
 
-    let nodes = $state.raw([
+    const initialNodes: Node[] = [
         {
             id: '1',
-            type: FlowNodeType.UrlAction,
-            data: { label: 'Hello' },
-            position: { x: 0, y: 0 },
+            type: 'input',
+            data: { label: 'An input node' },
+            position: { x: 0, y: 50 },
+            sourcePosition: Position.Right,
         },
         {
             id: '2',
-            data: { label: 'World' },
-            position: { x: 150, y: 150 },
+            type: FlowNodeType.UrlAction,
+            data: {},
+            position: { x: 300, y: 50 },
         },
-    ]);
-
-    let edges = $state.raw([
         {
-            id: '1-2',
+            id: '3',
+            type: FlowNodeType.Output,
+            data: { label: 'Output A' },
+            position: { x: 650, y: 25 },
+            targetPosition: Position.Left,
+        },
+        {
+            id: '4',
+            type: FlowNodeType.Output,
+            data: { label: 'Output B' },
+            position: { x: 650, y: 100 },
+            targetPosition: Position.Left,
+        },
+    ];
+
+    const initialEdges: Edge[] = [
+        {
+            id: 'e1-2',
             source: '1',
             target: '2',
+            animated: true,
         },
-    ]);
+        {
+            id: 'e2a-3',
+            source: '2',
+            target: '3',
+            animated: true,
+        },
+        {
+            id: 'e2b-4',
+            source: '2',
+            target: '4',
+            animated: true,
+        },
+    ];
+
+    let nodes = $state.raw<Node[]>(initialNodes);
+    let edges = $state.raw<Edge[]>(initialEdges);
 </script>
 
 <div class="w-full h-full">
-    <SvelteFlow bind:nodes nodeTypes={additionalFlowNodes} bind:edges fitView>
+    <SvelteFlow
+            bind:nodes
+            bind:edges
+            nodeTypes={additionalFlowNodes}
+            fitView
+
+    >
         <Controls />
         <Background />
         <MiniMap />
