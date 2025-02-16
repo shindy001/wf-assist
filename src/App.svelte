647183a70ded1a3c9b8acbde9cda05fd4d771056
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-16 18:09:00 +0100

    refactor: extract svelteflow to Landing component

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,10 @@
-<script lang="ts">
-  import {
-    SvelteFlow,
-    Controls,
-    Background,
-    MiniMap,
-  } from '@xyflow/svelte';
-
-  import '@xyflow/svelte/dist/style.css';
-
-  let nodes = $state.raw([
-    {
-      id: '1',
-      data: { label: 'Hello' },
-      position: { x: 0, y: 0 },
-    },
-    {
-      id: '2',
-      data: { label: 'World' },
-      position: { x: 150, y: 150 },
-    },
-  ]);
-
-  let edges = $state.raw([
-    {
-      id: '1-2',
-      source: '1',
-      target: '2',
-    },
-  ]);
+<script>
+  import Landing from "./landing/Landing.svelte";
 </script>
 
-<div style:height="100vh">
-  <SvelteFlow bind:nodes bind:edges fitView>
-    <Controls />
-    <Background />
-    <MiniMap />
-  </SvelteFlow>
+<div style:height="100vh" style:width="100vw">
+  <!-- Header -->
+  <!-- Sidebar -->
+  <Landing /> <!-- Content -->
+  <!-- Footer -->
 </div>
---
+++ src/landing/Landing.svelte
@@ -0,0 +1,39 @@
+﻿<script lang="ts">
+    import {
+        SvelteFlow,
+        Controls,
+        Background,
+        MiniMap,
+    } from '@xyflow/svelte';
+
+    import '@xyflow/svelte/dist/style.css';
+
+    let nodes = $state.raw([
+        {
+            id: '1',
+            data: { label: 'Hello' },
+            position: { x: 0, y: 0 },
+        },
+        {
+            id: '2',
+            data: { label: 'World' },
+            position: { x: 150, y: 150 },
+        },
+    ]);
+
+    let edges = $state.raw([
+        {
+            id: '1-2',
+            source: '1',
+            target: '2',
+        },
+    ]);
+</script>
+
+<div class="w-full h-full">
+    <SvelteFlow bind:nodes bind:edges fitView>
+        <Controls />
+        <Background />
+        <MiniMap />
+    </SvelteFlow>
+</div>
