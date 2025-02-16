9108634166725b2e7154714bc867ee0d1d828e74
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-16 18:53:34 +0100

    feat: introduce custom node UrlActionNode

---
+++ src/components/UrlActionNode.svelte
@@ -0,0 +1,13 @@
+﻿<script lang="ts">
+    import { Handle, Position, type NodeProps} from "@xyflow/svelte";
+
+</script>
+
+<div>
+    <div>
+        This is and UrlAction Node
+    </div>
+    <Handle type="target" position={Position.Left} />
+    <Handle type="source" position={Position.Right} />
+</div>
+
