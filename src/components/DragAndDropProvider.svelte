817b766bbbb60b163bb849998a8a6be8bc5bcb9b
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-17 00:21:02 +0100

    feat: introduce DragAndDropProvider component

---
+++ src/components/DragAndDropProvider.svelte
@@ -0,0 +1,29 @@
+﻿<script module lang="ts">
+    export const useDragAndDrop = () => {
+        return getContext('DragAndDrop') as { current: string | null };
+    }
+</script>
+
+<script lang="ts">
+    import {getContext, onDestroy, setContext, type Snippet} from "svelte";
+
+    let {children}: { children: Snippet } = $props();
+    let dragAndDropType = $state(null);
+
+    setContext('DragAndDrop', {
+        set current(value) {
+            dragAndDropType = value;
+        },
+        get current() {
+            return dragAndDropType;
+        }
+    });
+
+    onDestroy(() => {
+        if (dragAndDropType) {
+            dragAndDropType = null;
+        }
+    })
+</script>
+
+{@render children()}
