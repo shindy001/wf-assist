4002b841c5d5a0ebc9d9315e1f89458bd2780622
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-16 18:53:09 +0100

    feat: introduce FlowNodeType enum

---
+++ src/components/FlowNodeType.ts
@@ -0,0 +1,10 @@
+﻿export enum FlowNodeType {
+    // Default node types - https://reactflow.dev/api-reference/types/node#default-node-types
+    Default = "default",
+    Input = "input",
+    Output = "output",
+    Group = "group",
+
+    // Custom node types
+    UrlAction = "urlAction",
+}
