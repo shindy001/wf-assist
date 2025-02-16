883fc45341fc1d574f01cce0ad77a654dc22265b
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-16 21:04:14 +0100

    feat: implement localStorage flowDataStore

---
+++ src/stores/flowDataStore.ts
@@ -0,0 +1,40 @@
+﻿import type {Edge, Node} from "@xyflow/svelte";
+import {get, writable} from "svelte/store";
+
+export interface FlowData {
+    nodes: Node[],
+    edges: Edge[],
+}
+
+export function useFlowDataStore() {
+    const store = writable<FlowData>();
+    const isBrowser = typeof window !== "undefined";
+    const flowDataKey = "flowData";
+
+    return {
+        setData: (data: FlowData) => {
+            if (!isBrowser) {
+                throw new Error("FlowDataStore cannot be used outside of browser")
+            }
+            localStorage.setItem(flowDataKey, JSON.stringify(data));
+            store.set(data);
+        },
+        getData: () => {
+            if (!isBrowser) {
+                throw new Error("FlowDataStore cannot be used outside of browser")
+            }
+
+            let data = get(store);
+            if (data) {
+                return data;
+            }
+
+            const json = localStorage.getItem(flowDataKey);
+            if (json) {
+                data = JSON.parse(json);
+            }
+            store.set(data);
+            return get(store);
+        }
+    }
+}
