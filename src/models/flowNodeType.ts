eb520b2810f4440a3e0b39816c3f6f9b9edf04d6
shindy001 <sindelar.p.etr@seznam.cz>
2025-02-19 00:14:32 +0100

    refactor: comment out default and unsupported group FlowNode types

--- src/models/flowNodeType.ts
+++ src/models/flowNodeType.ts
@@ -1,9 +1,12 @@
 ﻿export enum FlowNodeType {
     // Default node types - https://reactflow.dev/api-reference/types/node#default-node-types
-    Default = "default",
+
+    // "default" - default node is a fallback when node is not specified, no need an enum value for it
+    // "group" - Used for grouping, group is a parent node that contains other nodes and moves together,
+    // grouping is not implemented right now, more info https://svelteflow.dev/learn/guides/sub-flows
+
     Input = "input",
     Output = "output",
-    Group = "group",
 
     // Custom node types
     UrlAction = "urlAction",
