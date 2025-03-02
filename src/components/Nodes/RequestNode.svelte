55976d78e126ad1568b146e6e606b210c071751f
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-02 23:22:46 +0100

    fix: move custom node pin color to RequestNode

--- src/components/Nodes/RequestNode.svelte
+++ src/components/Nodes/RequestNode.svelte
@@ -86,7 +86,7 @@
         <div class="">
             <div class="relative flex justify-end">
                 <p class="mr-4">Result value</p>
-                <Handle id="result" class="node-pin" type="source" position={Position.Right}/>
+                <Handle id="result" class="node-pin !bg-blue-300" type="source" position={Position.Right}/>
             </div>
         </div>
     </div>
