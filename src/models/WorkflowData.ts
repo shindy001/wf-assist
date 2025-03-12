476baec8649bd56748b24d13ce6d24ba30869752
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-12 01:03:08 +0100

    feat: introduce WorkflowData model

---
+++ src/models/WorkflowData.ts
@@ -0,0 +1,20 @@
+﻿export enum ExecutionStatus {
+    Waiting,
+    Started,
+    Succeeded,
+    Failed
+}
+
+export type ExecutionItem = {
+    id: string;
+    type?: string;
+    data?: any;
+    status: ExecutionStatus;
+}
+
+export interface WorkflowData {
+    executionList: Array<ExecutionItem>;
+    executingItem: string | undefined;
+    status: ExecutionStatus;
+    results: Record<string, {} | undefined>
+}
