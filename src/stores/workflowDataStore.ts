960468474c9fc62c9117fe2b66d9dac96abd2a16
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-12 01:04:45 +0100

    feat: introduce workflowDataStore

---
+++ src/stores/workflowDataStore.ts
@@ -0,0 +1,36 @@
+﻿import {get, writable} from "svelte/store";
+import {isBrowser} from "../utils/platformUtils";
+import type {WorkflowData} from "../models/WorkflowData";
+
+const workflowDataStore = writable<WorkflowData>();
+
+export function useWorkflowDataStore() {
+    const workflowData = "workflowData";
+
+    return {
+        setData: (data: WorkflowData) => {
+            if (!isBrowser()) {
+                throw new Error("WorkflowDataStore cannot be used outside of a browser")
+            }
+            localStorage.setItem(workflowData, JSON.stringify(data));
+            workflowDataStore.set(data);
+        },
+        getData: () => {
+            if (!isBrowser()) {
+                throw new Error("FlowDataStore cannot be used outside of a browser")
+            }
+
+            let data = get(workflowDataStore);
+            if (data) {
+                return data;
+            }
+
+            const json = localStorage.getItem(workflowData);
+            if (json) {
+                data = JSON.parse(json);
+            }
+            workflowDataStore.set(data);
+            return get(workflowDataStore);
+        }
+    }
+}
