87eed660ebfa315c90be63b5788caf1e651d165e
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-12 01:04:59 +0100

    feat: introduce printStringNodeProcessor

---
+++ src/processors/nodes/printStringNodeProcessor.ts
@@ -0,0 +1,26 @@
+﻿import {useWorkflowDataStore} from "../../stores/workflowDataStore";
+import type {NodeProcessor} from "../../models/NodeProcessor";
+
+export interface PrintStringNode {
+    useLogger: boolean;
+    targetId: string | undefined;
+}
+
+export function usePrintStringNodeProcessor(): NodeProcessor<PrintStringNode> {
+    return {
+        // TODO - simplify and clean processing logic
+        process: (node: PrintStringNode) => {
+            if (node.targetId) {
+                const workflowDataStore = useWorkflowDataStore();
+                const workflowData = workflowDataStore.getData();
+                const targetResult = workflowData.results[node.targetId] ?? "Nothing to print.";
+
+                if (node.useLogger) {
+                    console.log(targetResult);
+                } else {
+                    // TODO - print somewhere else???
+                }
+            }
+        }
+    }
+}
