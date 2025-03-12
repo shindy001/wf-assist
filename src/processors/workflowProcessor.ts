b629745f0c4ab58e3b35813ffda0db82b82b50b5
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-12 01:05:09 +0100

    feat: introduce workflowProcessor

---
+++ src/processors/workflowProcessor.ts
@@ -0,0 +1,44 @@
+﻿import {ExecutionStatus, type WorkflowData} from "../models/WorkflowData";
+import {FlowNodeType} from "../models/FlowNodeType";
+import {usePrintStringNodeProcessor} from "./nodes/printStringNodeProcessor";
+import type {NodeProcessor} from "../models/NodeProcessor";
+
+const processorMap: Record<string, NodeProcessor<any>> = {
+    [FlowNodeType.PrintString]: usePrintStringNodeProcessor(),
+}
+
+export function useWorkflowProcessor() {
+    return {
+        // TODO - simplify and clean processing logic
+        process: (data: WorkflowData) => {
+            if (data.status !== ExecutionStatus.Waiting) {
+                // TODO - return or throw error???
+                console.error("Cannot start workflow, that is not in waiting state, workflow stat: " + data.status);
+                return;
+            }
+
+            data.status = ExecutionStatus.Started;
+
+            for (const executionItem of data.executionList) {
+                if (executionItem.type) {
+                    const processor: NodeProcessor<any> | undefined = processorMap[executionItem.type];
+                    if (!processor) {
+                        console.error("Aborting workflow run, cannot find workflow processor for node type: " + executionItem.type);
+                        executionItem.status = ExecutionStatus.Failed;
+                        //data.status = ExecutionStatus.Failed;
+                        //break;
+                        continue;
+                    }
+                    processor.process(executionItem.data);
+                    executionItem.status = ExecutionStatus.Succeeded;
+                } else {
+                    console.error("Aborting workflow run, empty node type: " + executionItem.type);
+                    data.status = ExecutionStatus.Failed;
+                }
+            }
+
+            data.status = ExecutionStatus.Succeeded;
+            return data;
+        }
+    }
+}
