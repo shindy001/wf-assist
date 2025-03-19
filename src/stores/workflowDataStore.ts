6d95852902cb3456c2c8da70fc0eaca2b64f2cfe
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-19 17:56:43 +0100

    feat: replace workflowDataStore impl with dexie db provider

--- src/stores/workflowDataStore.ts
+++ src/stores/workflowDataStore.ts
@@ -1,36 +1,90 @@
-﻿import {get, writable} from "svelte/store";
-import {isBrowser} from "../utils/platformUtils";
+﻿import {isBrowser} from "../utils/platformUtils";
 import type {WorkflowData} from "../models/WorkflowData";
+import {db, executeDbOperation} from "./db";
 
-const workflowDataStore = writable<WorkflowData>();
+export enum WorkflowDataStoreError {
+    AlreadyExists,
+    DoesNotExist,
+    Fatal
+}
+
+export type WorkflowDataError = {
+    error: WorkflowDataStoreError;
+    message: string;
+}
 
 export function useWorkflowDataStore() {
-    const workflowData = "workflowData";
+    if (!isBrowser()) {
+        throw new Error("WorkflowDataStore cannot be used outside of a browser");
+    }
+
+    const workflowExists = async (name: string): Promise<boolean | WorkflowDataError> => {
+        try {
+            const existingItem = await executeDbOperation(db.workflows.get(name));
+            return existingItem !== undefined;
+        } catch (error) {
+            return handleError(error);
+        }
+    }
+
+    const addWorkflow = async (data: WorkflowData): Promise<void | WorkflowDataError> => {
+        try {
+            if (await workflowExists(data.name)) {
+                return {
+                    error: WorkflowDataStoreError.AlreadyExists,
+                    message: `Workflow '${data.name}' already exists.`
+                };
+            }
+            await executeDbOperation(db.workflows.add(data));
+        } catch (error) {
+            return handleError(error);
+        }
+    }
+
+    const updateWorkflow = async (data: WorkflowData): Promise<void | WorkflowDataError> => {
+        try {
+            const itemExists = await workflowExists(data.name);
+            if (!itemExists) {
+                return {
+                    error: WorkflowDataStoreError.DoesNotExist,
+                    message: `Workflow '${data.name}' does not exist.`
+                };
+            }
+            await executeDbOperation(db.workflows.put(data));
+        } catch (error) {
+            return handleError(error);
+        }
+    }
+
+    const addOrUpdateWorkflow = async (data: WorkflowData): Promise<void | WorkflowDataError> => {
+        try {
+            await executeDbOperation(db.workflows.put(data));
+        } catch (error) {
+            return handleError(error);
+        }
+    }
+
+    const deleteWorkflow = async (name: string): Promise<void | WorkflowDataError> => {
+        try {
+            const itemExists = await workflowExists(name);
+            if (itemExists) {
+                await db.workflows.delete(name);
+            }
+        } catch (error) {
+            return handleError(error);
+        }
+    }
+
+    const handleError = (error: unknown): WorkflowDataError => {
+        const message = error instanceof Error ? error.message : 'Unknown error';
+        return {error: WorkflowDataStoreError.Fatal, message: message};
+    };
 
     return {
-        setData: (data: WorkflowData) => {
-            if (!isBrowser()) {
-                throw new Error("WorkflowDataStore cannot be used outside of a browser")
-            }
-            localStorage.setItem(workflowData, JSON.stringify(data));
-            workflowDataStore.set(data);
-        },
-        getData: () => {
-            if (!isBrowser()) {
-                throw new Error("FlowDataStore cannot be used outside of a browser")
-            }
-
-            let data = get(workflowDataStore);
-            if (data) {
-                return data;
-            }
-
-            const json = localStorage.getItem(workflowData);
-            if (json) {
-                data = JSON.parse(json);
-            }
-            workflowDataStore.set(data);
-            return get(workflowDataStore);
-        }
+        workflowExists,
+        addWorkflow,
+        updateWorkflow,
+        addOrUpdateWorkflow,
+        deleteWorkflow,
     }
 }
