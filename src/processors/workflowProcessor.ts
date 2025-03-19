ec57e7b731ac2b85d4f17ef4996799086a89caad
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-19 17:57:56 +0100

    feat: simplify workflowProcessor

--- src/processors/workflowProcessor.ts
+++ src/processors/workflowProcessor.ts
@@ -1,4 +1,4 @@
-﻿import {ExecutionStatus, type WorkflowData} from "../models/WorkflowData";
+﻿import {type WorkflowData} from "../models/WorkflowData";
 import {FlowNodeType} from "../models/nodes/FlowNodeType";
 import {usePrintStringNodeProcessor} from "./nodes/printStringNodeProcessor";
 import type {NodeProcessor} from "../models/nodes/NodeProcessor";
@@ -9,36 +9,23 @@ const processorMap: Record<string, NodeProcessor<any>> = {
 
 export function useWorkflowProcessor() {
     return {
-        // TODO - simplify and clean processing logic
         process: (data: WorkflowData) => {
-            if (data.status !== ExecutionStatus.Waiting) {
-                // TODO - return or throw error???
-                console.error("Cannot start workflow, that is not in waiting state, workflow stat: " + data.status);
-                return;
-            }
-
-            data.status = ExecutionStatus.Started;
+            console.info(`Starting workflow '${data.name}'."`);
 
             for (const executionItem of data.executionList) {
                 if (executionItem.type) {
                     const processor: NodeProcessor<any> | undefined = processorMap[executionItem.type];
                     if (!processor) {
                         console.error("Aborting workflow run, cannot find workflow processor for node type: " + executionItem.type);
-                        executionItem.status = ExecutionStatus.Failed;
-                        //data.status = ExecutionStatus.Failed;
-                        //break;
-                        continue;
+                        break;
                     }
                     processor.process(executionItem.data);
-                    executionItem.status = ExecutionStatus.Succeeded;
                 } else {
                     console.error("Aborting workflow run, empty node type: " + executionItem.type);
-                    data.status = ExecutionStatus.Failed;
                 }
             }
 
-            data.status = ExecutionStatus.Succeeded;
-            return data;
+            console.info(`Workflow '${data.name}' ended."`);
         }
     }
 }
