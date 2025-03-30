e2869bdf60c6875fa1b868e91c44ea0c29c11c68
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-30 11:13:19 +0200

    refactor: remove default SvelteFlow node types from FlowNodeType enum

--- src/models/nodes/FlowNodeType.ts
+++ src/models/nodes/FlowNodeType.ts
@@ -1,14 +1,8 @@
-﻿export enum FlowNodeType {
-    // Default node types - https://reactflow.dev/api-reference/types/node#default-node-types
-
-    // "default" - default node is a fallback when node is not specified, no need an enum value for it
-    // "group" - Used for grouping, group is a parent node that contains other nodes and moves together,
-    // grouping is not implemented right now, more info https://svelteflow.dev/learn/guides/sub-flows
-
-    Input = "input",
-    Output = "output",
-
-    // Custom node types
+﻿/**
+ * Custom node types
+ * @link {https://reactflow.dev/api-reference/types/node#default-node-types}
+ */
+export enum FlowNodeType {
     Request = "request",
     ExtractProperty = "extractProperty",
     PrintString = "printString",
