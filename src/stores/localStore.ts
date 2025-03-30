7fc7f9a6d0d52c431f80f912f581027469b08ba1
shindy001 <sindelar.p.etr@seznam.cz>
2025-03-31 01:05:15 +0200

    feat: replace flowDataStore with simple localStore

---
+++ src/stores/localStore.ts
@@ -0,0 +1,22 @@
+﻿import {isBrowser} from "../utils/platformUtils";
+
+export function useLocalStore() {
+    if (!isBrowser()) {
+        throw new Error("LocalStore cannot be used outside of browser")
+    }
+
+    return {
+        setItem: (key: string, data: any) => {
+            localStorage.setItem(key, JSON.stringify(data));
+        },
+        getItem: <TResult>(key: string): TResult | undefined => {
+            const json = localStorage.getItem(key);
+            let data: TResult | undefined = undefined;
+            if (json) {
+                data = JSON.parse(json);
+            }
+
+            return data;
+        }
+    }
+}
