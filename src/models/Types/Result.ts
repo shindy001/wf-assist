40171dfbdabb2997b74f72ee74eb952206378e53
shindy001 <sindelar.p.etr@seznam.cz>
2025-04-03 01:06:41 +0200

    feat: introduce Result type

---
+++ src/models/Types/Result.ts
@@ -0,0 +1,23 @@
+﻿import type {ErrorDetail} from "./ErrorDetail";
+
+export type Result<T = void> = {
+    isSuccessful: true;
+    data: T;
+    error: undefined;
+} | {
+    isSuccessful: false;
+    data: undefined;
+    error: ErrorDetail;
+};
+
+export const success = <T = void>(data: T): Result<T> => ({
+    isSuccessful: true,
+    data: data,
+    error: undefined,
+})
+
+export const failure = (error: ErrorDetail): Result<never> => ({
+    isSuccessful: false,
+    data: undefined,
+    error: error,
+})
