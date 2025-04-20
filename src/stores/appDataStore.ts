472bba77976727e63851e31ae1aa00535db92d4b
shindy001 <sindelar.p.etr@seznam.cz>
2025-04-21 01:37:22 +0200

    feat: implement appDataStore

---
+++ src/stores/appDataStore.ts
@@ -0,0 +1,18 @@
+﻿import {type Writable, writable} from "svelte/store";
+
+export interface AppData {
+    activeWorkflowId: string;
+}
+
+const appDataKey = "wfAssistAppData";
+const storedValue = localStorage.getItem(appDataKey);
+const initialValue: AppData = storedValue ? JSON.parse(storedValue) : { activeWorkflowId: undefined };
+const appDataStore = writable<AppData>(initialValue);
+
+appDataStore.subscribe((appData) => {
+    localStorage.setItem(appDataKey, JSON.stringify(appData));
+});
+
+export function useAppDataStore(): Writable<AppData> {
+    return appDataStore;
+}
