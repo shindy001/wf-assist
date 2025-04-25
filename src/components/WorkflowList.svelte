8c9be7e3175f28d9fa46cc6028d022c373e1c589
shindy001 <sindelar.p.etr@seznam.cz>
2025-04-25 22:04:49 +0200

    feat: show only workflow input when renaming, replace icon with save button

--- src/components/WorkflowList.svelte
+++ src/components/WorkflowList.svelte
@@ -137,7 +137,9 @@
             <button
                     class={[
                         "w-full px-2 flex gap-2 items-center content-center rounded-md cursor-pointer hover:bg-gray-100",
-                        workflow.name === $appDataStore.activeWorkflowName ? 'bg-gray-100' : ''
+                        workflow.name === $appDataStore.activeWorkflowName ? 'bg-gray-100' : '',
+                        workflow.name === contextMenuWorkflowName && showRenameInput ? 'hidden' : ''
+
                     ]}
                     onclick={() => setActiveWorkflow(workflow.name)}
                     oncontextmenu={showContextMenu}
@@ -152,11 +154,9 @@
 
             {#if workflow.name === contextMenuWorkflowName && showRenameInput}
                 <div class="relative">
-                    <input class="w-full px-2 pr-8 border border-black rounded-md" type="text" bind:value={contextMenuWorkflowRenameValue} required minlength="1">
-                    <button aria-label="rename" class="absolute z-10 right-1 top-[2px] rounded-md cursor-pointer hover:bg-gray-100 content-center items-center" onclick={renameWorkflow}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                        </svg>
+                    <input class="w-full px-2 pr-12 border border-black rounded-md" type="text" bind:value={contextMenuWorkflowRenameValue} required minlength="1">
+                    <button aria-label="rename" class="absolute z-10 right-[1px] top-[1px] px-1 rounded-md cursor-pointer bg-gray-300 hover:bg-gray-100 content-center items-center" onclick={renameWorkflow}>
+                        Save
                     </button>
                 </div>
 
