e88090cf68323a05847f7b563d606442ba034aa4
shindy001 <sindelar.p.etr@seznam.cz>
2025-05-11 23:07:33 +0200

    refactor: extract workflow related html in app to workflows component

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,7 @@
 <script>
-    import FlowSidebar from "./workflows/components/FlowSidebar.svelte";
-    import FlowCanvas from "./workflows/components/FlowCanvas.svelte";
-    import {SvelteFlowProvider} from "@xyflow/svelte";
-    import DragAndDropProvider from "./workflows/components/DragAndDropProvider.svelte";
+    import Workflows from "./workflows/Workflows.svelte";
 </script>
 
 <div id="content__root" style:height="100vh" style:width="100vw" class="relative">
-    <div class="flex w-full h-full">
-        <SvelteFlowProvider>
-            <DragAndDropProvider>
-                <FlowSidebar></FlowSidebar>
-                <FlowCanvas/>
-            </DragAndDropProvider>
-        </SvelteFlowProvider>
-    </div>
+    <Workflows/>
 </div>
---
+++ src/workflows/Workflows.svelte
@@ -0,0 +1,15 @@
+﻿<script>
+    import {SvelteFlowProvider} from "@xyflow/svelte";
+    import FlowSidebar from "./components/FlowSidebar.svelte";
+    import DragAndDropProvider from "./components/DragAndDropProvider.svelte";
+    import FlowCanvas from "./components/FlowCanvas.svelte";
+</script>
+
+<div class="flex w-full h-full">
+    <SvelteFlowProvider>
+        <DragAndDropProvider>
+            <FlowSidebar/>
+            <FlowCanvas/>
+        </DragAndDropProvider>
+    </SvelteFlowProvider>
+</div>
