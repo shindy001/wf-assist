33444d80a2f8a9aa142967f9c2889f5dac00732e
shindy001 <sindelar.p.etr@seznam.cz>
2025-06-06 01:52:37 +0200

    build(client): add base path and esnext build target to support top level awaits

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      svelte(),
-      tailwindcss(),
-  ],
+    base: './',
+    build: {
+        target: 'esnext',
+    },
+    plugins: [
+        svelte(),
+        tailwindcss(),
+    ],
 })
