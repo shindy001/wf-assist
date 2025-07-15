a28c729504455fece9b3045020edd9f93996bb62
shindy001 <sindelar.p.etr@seznam.cz>
2025-07-15 15:57:46 +0200

    feat(client): add mode watcher to support dark mode, changed default theme to neutral

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,9 +2,11 @@
     import "./app.css";
     import Workflows from "./workflows/Workflows.svelte";
     import Icons from "./lib/components/Icons.svelte";
+    import { ModeWatcher } from "mode-watcher";
 </script>
 
 <div id="content__root" style:height="100vh" style:width="100vw" class="relative">
+    <ModeWatcher />
     <Icons/>
     <Workflows/>
 </div>
