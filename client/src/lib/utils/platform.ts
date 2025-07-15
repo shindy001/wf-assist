12fd2607bd91a0f9939792b48ff0fbd604c7cb7f
shindy001 <sindelar.p.etr@seznam.cz>
2025-07-15 22:20:11 +0200

    feat(client): remove platform compatibilty check

--- client/src/lib/utils/platform.ts
+++
@@ -1,4 +0,0 @@
-﻿export const isCompatiblePlatform =
-    () => typeof window !== 'undefined' &&
-        typeof localStorage !== 'undefined' &&
-        typeof indexedDB !== 'undefined';
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,6 @@
 import App from './App.svelte';
 
 import {mount} from "svelte";
-import {isCompatiblePlatform} from "./lib/utils/platform";
-
-if (!isCompatiblePlatform()) {
-  throw new Error("Unsupported platform. This application requires localStorage and indexDB to run hence cannot run outside of a browser.")
-}
 
 const app = mount(App, {
   target: document.getElementById('app')!,
