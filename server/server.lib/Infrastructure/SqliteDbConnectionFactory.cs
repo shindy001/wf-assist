410062ac9b5def21d5a6538875e2f0e2ad1a62ee
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-10 01:39:02 +0200

    feat(server.lib): introduce SqliteDbConnectionFactory and IDbConnectionFactory contract

---
+++ server/server.lib/Infrastructure/SqliteDbConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace WfAssist.AspNetCore.Infrastructure;
+
+public interface IDbConnectionFactory
+{
+    public IDbConnection CreateConnection();
+}
+
+internal sealed class SqliteDbConnectionFactory : IDbConnectionFactory
+{
+    public IDbConnection CreateConnection() => new SqliteConnection(Constants.SqliteDbConnectionString);
+}
