00c45db2d8c9ab2116beb82daca90b466b409476
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-10 01:38:17 +0200

    feat(server.lib): add SqliteDbConnectionString value to Constants

--- server/server.lib/Constants.cs
+++ server/server.lib/Constants.cs
@@ -10,4 +10,18 @@ internal static class Constants
     public const string IndexCssFile = "index.css";
     public const string IndexJsFile = "index.js";
     public const string FaviconFile = "favicon.ico";
+
+    public static readonly string SqliteDbConnectionString = GetSqliteConnectionString();
+
+    private static string GetSqliteConnectionString()
+    {
+        var dbPath = Path.Combine(AppContext.BaseDirectory, "data", "wf-assist.db");
+
+        if (!Directory.Exists(dbPath))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(dbPath)!);
+        }
+
+        return $"Data Source={dbPath}";
+    }
 }
