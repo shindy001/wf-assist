d0e763378358d06dbea287b169d2d3a09dc5e096
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-10 01:40:33 +0200

    feat(server.lib): add CreateWorkflowTable migration

---
+++ server/server.lib/Infrastructure/Migrations/CreateWorkflowTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace WfAssist.AspNetCore.Infrastructure.Migrations;
+
+[Migration(2025100901)]
+public class CreateWorkflowTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Workflows")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Workflows");
+    }
+}
