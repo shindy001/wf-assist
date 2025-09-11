071bb283b9d07076b8b2aff6a697edfb8c6a0eb2
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-12 01:12:32 +0200

    feat(server.lib): introduce CreateReadonlyConnection to SqliteDbConnectionFactory

--- server/server.lib/Infrastructure/SqliteDbConnectionFactory.cs
+++ server/server.lib/Infrastructure/SqliteDbConnectionFactory.cs
@@ -6,9 +6,19 @@ namespace WfAssist.AspNetCore.Infrastructure;
 public interface IDbConnectionFactory
 {
     public IDbConnection CreateConnection();
+    public IDbConnection CreateReadonlyConnection();
 }
 
 internal sealed class SqliteDbConnectionFactory : IDbConnectionFactory
 {
     public IDbConnection CreateConnection() => new SqliteConnection(Constants.SqliteDbConnectionString);
+    public IDbConnection CreateReadonlyConnection()
+    {
+        var builder = new SqliteConnectionStringBuilder(Constants.SqliteDbConnectionString)
+        {
+            Mode = SqliteOpenMode.ReadOnly
+        };
+
+        return new SqliteConnection(builder.ToString());
+    }
 }
