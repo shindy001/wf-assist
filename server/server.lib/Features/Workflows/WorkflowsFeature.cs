22eade5fd11daf9d5f05d1ba09f1fd781fd9dcc1
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-14 00:48:40 +0200

    feat(server.lib): add JetBrains.Annotations package and mark WorkflowFeature with UsedImplicitly attribute

--- server/server.lib/Features/Workflows/WorkflowsFeature.cs
+++ server/server.lib/Features/Workflows/WorkflowsFeature.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using WfAssist.AspNetCore.Shared;
 
 namespace WfAssist.AspNetCore.Features.Workflows;
 
+[UsedImplicitly]
 internal sealed class WorkflowsFeature : IFeatureModule
 {
     public void RegisterServices(IServiceCollection services)
