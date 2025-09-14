a043e8f77843e08b562c1c312e53f4b3cdc45fdc
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-15 00:36:45 +0200

    feat(server.lib): introduce GetIdentities endpoint and response model

---
+++ server/server.lib/Features/Workflows/GetIdentities/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace WfAssist.AspNetCore.Features.Workflows.GetIdentities;
+
+public static class Endpoint
+{
+    public static void MapGetIdentitiesEndpoint(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/identities", () =>
+            {
+                // TODO
+                // 1. Create handler and register to services
+                // 2. inject handler and from services and call
+                return TypedResults.Ok(new GetIdentitiesResponse());
+            })
+            .Produces<GetIdentitiesResponse>();
+    }
+}
---
+++ server/server.lib/Features/Workflows/GetIdentities/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace WfAssist.AspNetCore.Features.Workflows.GetIdentities;
+
+public record WorkFlowIdentityDto(string Id, string Name);
+
+public record GetIdentitiesResponse
+{
+    public IEnumerable<WorkFlowIdentityDto> Identities { get; init; } = [];
+}
