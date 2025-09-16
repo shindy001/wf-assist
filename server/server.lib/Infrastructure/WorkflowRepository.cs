918ea47bc80428c48665da4195ed53b87c8f5ceb
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-16 23:55:45 +0200

    feat(server.lib): implement WorkflowRepository

---
+++ server/server.lib/Infrastructure/WorkflowRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using Dapper;
+using WfAssist.AspNetCore.Domain.Workflows.Contracts;
+using WfAssist.AspNetCore.Domain.Workflows.Models;
+using WfAssist.AspNetCore.Shared;
+
+namespace WfAssist.AspNetCore.Infrastructure;
+
+public sealed class WorkflowRepository : IWorkflowRepository
+{
+    private readonly IDbConnection _dbConnection;
+
+    public WorkflowRepository(IDbConnectionProvider dbConnectionProvider)
+    {
+        _dbConnection = dbConnectionProvider.DbConnection;
+    }
+
+    public async Task<IEnumerable<WorkflowIdentity>> GetIdentities()
+    {
+        const string sql = "SELECT id, name FROM Workflows";
+
+        return await _dbConnection.QueryAsync<WorkflowIdentity>(sql);
+    }
+
+    public async Task<Workflow?> GetById(Guid id)
+    {
+        const string sql = "SELECT * FROM Workflows WHERE Id = @id";
+
+        return await _dbConnection.QuerySingleOrDefaultAsync<Workflow>(sql, new { id });
+    }
+
+    public async Task<bool> Exists(Guid id)
+    {
+        const string sql = "SELECT COUNT(1) FROM Workflows WHERE Id = @id";
+
+        return await _dbConnection.ExecuteScalarAsync<bool>(sql, new { id });
+    }
+
+    public async Task Create(Workflow workflow)
+    {
+        const string sql = "INSERT INTO Workflows (Id, Name, Data) VALUES (@Id, @Name, @Data)";
+
+        await _dbConnection.ExecuteAsync(sql, workflow);
+    }
+
+    public async Task Rename(Guid id, string newName)
+    {
+        const string sql = "UPDATE Workflows SET Name = @Name WHERE Id = @Id";
+
+        await _dbConnection.ExecuteAsync(sql, new {Id = id, Name = newName});
+    }
+
+    public async Task UpdateData(Guid id, WorkflowData data)
+    {
+        const string sql = "UPDATE Workflows SET Data = @Data WHERE Id = @Id";
+
+        await _dbConnection.ExecuteAsync(sql, data);
+    }
+
+    public async Task Delete(Guid id)
+    {
+        const string sql = "DELETE FROM Workflows WHERE Id = @Id";
+
+        if (await Exists(id))
+        {
+            await _dbConnection.ExecuteAsync(sql, new {id});
+        }
+    }
+}
