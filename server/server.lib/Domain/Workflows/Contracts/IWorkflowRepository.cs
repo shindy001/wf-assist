e4376f02467a7570476962c1f98a5f19aa826b46
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-16 23:52:56 +0200

    feat(server.lib): introduce IWorkflowRepository contract

---
+++ server/server.lib/Domain/Workflows/Contracts/IWorkflowRepository.cs
@@ -0,0 +1,15 @@
+﻿using WfAssist.AspNetCore.Domain.Workflows.Models;
+
+namespace WfAssist.AspNetCore.Domain.Workflows.Contracts;
+
+public interface IWorkflowRepository
+{
+    Task<IEnumerable<WorkflowIdentity>> GetIdentities();
+    Task<Workflow?> GetById(Guid id);
+    Task<bool> Exists(Guid workflowId);
+    Task Create(Workflow workflow);
+    Task Rename(Guid id, string newName);
+    Task UpdateData(Guid workflowId, WorkflowData data);
+    Task Delete(Guid id);
+}
+
