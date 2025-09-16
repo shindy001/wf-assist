e7610498ae22059dcfef6b317f63027db66534f2
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-16 23:54:42 +0200

    feat(server.lib): introduce DbJsonSerializerOptions

---
+++ server/server.lib/Infrastructure/Serialization/DbJsonSerializerOptions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WfAssist.AspNetCore.Infrastructure.Serialization;
+
+public static class DbJsonSerializerOptions
+{
+    public static JsonSerializerOptions Value { get; }
+
+    static DbJsonSerializerOptions()
+    {
+        Value = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        };
+
+        Value.Converters.Add(new JsonStringEnumConverter());
+    }
+
+
+}
