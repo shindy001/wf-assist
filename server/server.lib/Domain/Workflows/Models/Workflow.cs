563796942a337282b2670f60acd0e9a673b7870e
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-17 11:57:01 +0200

    refactor(server.lib): simplify workflow model by making workflow node abstract

--- server/server.lib/Domain/Workflows/Models/Workflow.cs
+++ server/server.lib/Domain/Workflows/Models/Workflow.cs
@@ -13,18 +13,37 @@ public sealed record WorkflowData
     public IEnumerable<WorkflowEdge> Edges { get; init; } = [];
 }
 
-public sealed record WorkflowNode
-{
-    public required string Id { get; init; }
-    public required WorkflowNodeType Type { get; init; }
-    public required Position Position { get; init; }
-    public required WorkflowNodeData Data { get; init; }
-}
-
 public sealed record WorkflowEdge
 {
     public required string Id { get; init; }
     public required Position Position { get; init; }
     public required string Source { get; init; }
     public required string Target { get; init; }
+}
+
+
+public abstract record WorkflowNode(WorkflowNodeType Type)
+{
+    public required string Id { get; init; }
+    public required Position Position { get; init; }
+}
+
+public sealed record PrintTextNode() : WorkflowNode(WorkflowNodeType.PrintText)
+{
+    public required string Text { get; init; }
+    public bool UseConsole { get; init; }
+    public string? TargetId { get; init; }
+}
+
+public sealed record RequestNode() : WorkflowNode(WorkflowNodeType.Request)
+{
+    public required string RequestType { get; init; }
+    public required string Url { get; init; }
+    public string? RequestBody { get; init; }
+}
+
+public sealed record ExtractPropertyNode() : WorkflowNode(WorkflowNodeType.ExtractProperty)
+{
+    public required string Path { get; init; }
+    public required string TargetId { get; init; }
 }
