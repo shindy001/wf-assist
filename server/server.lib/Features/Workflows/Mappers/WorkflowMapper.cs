f855e6a6355ea950c2303d0e2f514fbc23cc2dee
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-17 13:28:21 +0200

    feat(server.lib): add ToDto mappings to WorkflowMapper

--- server/server.lib/Features/Workflows/Mappers/WorkflowMapper.cs
+++ server/server.lib/Features/Workflows/Mappers/WorkflowMapper.cs
@@ -5,11 +5,43 @@ namespace WfAssist.AspNetCore.Features.Workflows.Mappers;
 
 public static class WorkflowMapper
 {
+    public static WorkflowDto ToDto(this Workflow entity)
+        => new()
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            Data = entity.Data.ToDto()
+        };
+
+    public static Workflow ToDomain(this WorkflowDto dto)
+        => new()
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Data = dto.Data.ToDomain()
+        };
+
+    public static WorkflowDataDto ToDto(this WorkflowData entity)
+        => new()
+        {
+            Edges = entity.Edges.Select(ToDto),
+            Nodes = entity.Nodes.Select(ToDto)
+        };
+
     public static WorkflowData ToDomain(this WorkflowDataDto dto)
         => new()
         {
             Edges = dto.Edges.Select(ToDomain),
             Nodes = dto.Nodes.Select(ToDomain)
+        };
+
+    public static WorkflowEdgeDto ToDto(this WorkflowEdge entity)
+        => new()
+        {
+            Id = entity.Id,
+            Position = entity.Position.ToDto(),
+            Source = entity.Source,
+            Target = entity.Target,
         };
 
     public static WorkflowEdge ToDomain(this WorkflowEdgeDto dto)
@@ -21,17 +53,48 @@ public static class WorkflowMapper
             Target = dto.Target,
         };
 
+    public static WorkflowNodeDto ToDto(this WorkflowNode entity)
+    {
+        return entity switch
+        {
+            PrintTextNode printTextNode => new PrintTextNodeDto
+            {
+                Id = printTextNode.Id,
+                Position = printTextNode.Position.ToDto(),
+                Text = printTextNode.Text,
+                UseConsole = printTextNode.UseConsole,
+                TargetId = printTextNode.TargetId
+            },
+            RequestNode requestNode => new RequestNodeDto
+            {
+                Id = requestNode.Id,
+                Position = requestNode.Position.ToDto(),
+                RequestType = requestNode.RequestType,
+                Url = requestNode.Url,
+                RequestBody = requestNode.RequestBody
+            },
+            ExtractPropertyNode extractPropertyNode => new ExtractPropertyNodeDto
+            {
+                Id = extractPropertyNode.Id,
+                Position = extractPropertyNode.Position.ToDto(),
+                Path = extractPropertyNode.Path,
+                TargetId = extractPropertyNode.TargetId
+            },
+            _ => throw new InvalidOperationException($"Unknown WorkflowNodeDto type {entity.GetType().Name}")
+        };
+    }
+
     public static WorkflowNode ToDomain(this WorkflowNodeDto dto)
     {
         return dto switch
         {
-            PrintTextNodeDto printTextDto => new PrintTextNode
+            PrintTextNodeDto printTextNodeDto => new PrintTextNode
             {
-                Id = printTextDto.Id,
-                Position = printTextDto.Position.ToDomain(),
-                Text = printTextDto.Text,
-                UseConsole = printTextDto.UseConsole,
-                TargetId = printTextDto.TargetId
+                Id = printTextNodeDto.Id,
+                Position = printTextNodeDto.Position.ToDomain(),
+                Text = printTextNodeDto.Text,
+                UseConsole = printTextNodeDto.UseConsole,
+                TargetId = printTextNodeDto.TargetId
             },
             RequestNodeDto requestNodeDto => new RequestNode
             {
@@ -52,5 +115,6 @@ public static class WorkflowMapper
         };
     }
 
+    public static PositionDto ToDto(this Position entity) => new(entity.X, entity.Y);
     public static Position ToDomain(this PositionDto dto) => new(dto.X, dto.Y);
 }
