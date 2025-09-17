7759104104b9ad343a620ceac5946c7383c12a3b
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-17 12:30:13 +0200

    feat(server.lib): implement Workflow create action with all the data

--- server/server.lib/Features/Workflows/Create.cs
+++ server/server.lib/Features/Workflows/Create.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using WfAssist.AspNetCore.Domain.Workflows.Contracts;
 using WfAssist.AspNetCore.Domain.Workflows.Models;
+using WfAssist.AspNetCore.Features.Workflows.Dtos;
+using WfAssist.AspNetCore.Features.Workflows.Mappers;
 
 namespace WfAssist.AspNetCore.Features.Workflows;
 
@@ -12,18 +14,20 @@ public static class Create
     {
         endpoints.MapPost("/", async (CreateWorkflowRequest request, IWorkflowRepository workflowRepository) =>
             {
-                await workflowRepository.Create(new Workflow
+                var newWorkflow = new Workflow
                 {
                     Id = Guid.NewGuid(),
                     Name = request.Name,
-                    Data = new WorkflowData() // TODO - Define WorkflowData model for CreateWorkflowRequest and map to internal WorkflowData
-                });
+                    Data = request.Data.ToDomain()
+                };
+
+                await workflowRepository.Create(newWorkflow);
 
                 return TypedResults.NoContent();
             })
             .Produces(StatusCodes.Status204NoContent);
     }
 
-    private sealed record CreateWorkflowRequest(string Name);
+    private sealed record CreateWorkflowRequest(string Name, WorkflowDataDto Data);
 
 }
