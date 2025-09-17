afbb9f501d78506c4fc2da9dd0640d1ebbb14643
shindy001 <sindelar.p.etr@seznam.cz>
2025-09-17 11:57:48 +0200

    feat(server.host): configure json http options

--- server/server.host/Program.cs
+++ server/server.host/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.OpenApi.Models;
 using Scalar.AspNetCore;
 using WfAssist.AspNetCore;
@@ -5,6 +8,15 @@ using WfAssist.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddWfAssistServices();
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    options.SerializerOptions.WriteIndented = true;
+    options.SerializerOptions.AllowTrailingCommas = true;
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 builder.Services.AddOpenApi(opt =>
 {
