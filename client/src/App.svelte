db92932d1b834b386bd84cd86148c8fc2e5a6ab6
shindy001 <sindelar.p.etr@seznam.cz>
2025-10-05 23:25:52 +0200

    feat(client): initialize tanstack svelte query client in App component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,10 +3,15 @@
     import Workflows from "./workflows/Workflows.svelte";
     import { Icons } from "$lib/components/ui/icons";
     import { ModeWatcher } from "mode-watcher";
+    import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+
+    const queryClient = new QueryClient();
 </script>
 
 <div style:height="100vh" style:width="100vw" class="relative">
-    <ModeWatcher />
-    <Icons/>
-    <Workflows/>
+    <QueryClientProvider client={queryClient}>
+        <ModeWatcher />
+        <Icons/>
+        <Workflows/>
+    </QueryClientProvider>
 </div>
