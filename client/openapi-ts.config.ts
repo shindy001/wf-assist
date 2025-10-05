e1732be588c4fbdebd98d52b2f0f53fe2d593762
shindy001 <sindelar.p.etr@seznam.cz>
2025-10-05 23:24:44 +0200

    feat(client): add tanstack svelte query package, extract generate-api-client config to openapi-ts.config.ts

---
+++ client/openapi-ts.config.ts
@@ -0,0 +1,17 @@
+﻿import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+    input: "./../server/server.host/wfAssist-api.json",
+    output: {
+        path: "src/api",
+        lint: null,
+    },
+    plugins: [
+        {
+            name: "@tanstack/svelte-query",
+            queryOptions: {
+                name: "{{name}}Query",
+            },
+        },
+    ],
+});
