574e18c228b36431d5174bb46fdc992127034226
shindy001 <sindelar.p.etr@seznam.cz>
2025-10-05 22:02:08 +0200

    feat(client): set baseUrl for api client in main.ts, correct api addresses in env files

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,11 @@
 import {mount} from "svelte";
 import App from './App.svelte';
+import { client } from '$api/client.gen';
+
+// api client config, client is available at /src/api
+client.setConfig({
+    baseUrl: import.meta.env.VITE_API_ADDRESS,
+});
 
 const app = mount(App, {
   target: document.getElementById("app")!,
