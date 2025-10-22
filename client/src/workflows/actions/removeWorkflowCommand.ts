cc4abe6f94facd1837285d530f097308ec9e7648
shindy001 <sindelar.p.etr@seznam.cz>
2025-10-23 01:49:16 +0200

    feat(client): implement remove workflow command

--- client/src/workflows/actions/removeWorkflowCommand.ts
+++ client/src/workflows/actions/removeWorkflowCommand.ts
@@ -1,5 +1,12 @@
-﻿export function createRemoveWorkflowCommand() {
-    return async (workflowName: string)=> {
-        // TODO - send delete request to server
+﻿import {deleteWfAssistWorkflowsById} from "$api";
+
+export function createRemoveWorkflowCommand() {
+    return async (workflowId: string)=> {
+        const result = await deleteWfAssistWorkflowsById({ path: { id: workflowId } });
+
+        if (result.error) {
+            // TODO - consume with some error service and show message box or something like that???
+            console.error(result.error);
+        }
     }
 }
