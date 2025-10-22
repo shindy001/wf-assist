ed71dd9015ce8f76d3990990ae715cf07b8db2fd
shindy001 <sindelar.p.etr@seznam.cz>
2025-10-22 22:36:53 +0200

    fix(client): remove remains of tanstack query

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,15 +3,10 @@
     import Workflows from "./workflows/Workflows.svelte";
     import { Icons } from "$lib/components/ui/icons";
     import { ModeWatcher } from "mode-watcher";
-    import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
-
-    const queryClient = new QueryClient();
 </script>
 
 <div style:height="100vh" style:width="100vw" class="relative">
-    <QueryClientProvider client={queryClient}>
-        <ModeWatcher />
-        <Icons/>
-        <Workflows/>
-    </QueryClientProvider>
+    <ModeWatcher />
+    <Icons/>
+    <Workflows/>
 </div>
