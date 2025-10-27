b3b315ae8cb878c4d8aa1991b087c9731ccaa22a
shindy001 <sindelar.p.etr@seznam.cz>
2025-10-27 02:09:46 +0100

    feat(client): implement saveWorkflowCommand

--- client/src/workflows/actions/saveWorkflowCommand.ts
+++ client/src/workflows/actions/saveWorkflowCommand.ts
@@ -1,7 +1,25 @@
+import {
+  postWfAssistWorkflowsByIdUpdateData,
+  type WorkflowNodeDto,
+} from "$api";
+import { failed, successful, type WorkflowData } from "$lib/components/types";
 import { throttle } from "lodash";
 
 export function createSaveWorkflowCommand(saveRateLimitInMilliseconds: number) {
-  return throttle(async (workflowData: unknown) => {
-    // TODO - send save request to server
+  return throttle(async (id: string, workflowData: WorkflowData) => {
+    const result = await postWfAssistWorkflowsByIdUpdateData({
+      path: {
+        id: id,
+      },
+      body: {
+        data: {
+          nodes: workflowData?.nodes?.map(
+            (x) => x as unknown as WorkflowNodeDto,
+          ),
+          edges: workflowData.edges,
+        },
+      },
+    });
+    return result.error ? failed(result.error.toString()) : successful();
   }, saveRateLimitInMilliseconds);
 }
