00bca40ed100cb9b501cd096ad632394daf83d42
shindy001 <sindelar.p.etr@seznam.cz>
2025-11-02 23:26:49 +0100

    feat(client): correct RequestNode handle according to other ones

--- client/src/workflows/components/nodes/RequestNode.svelte
+++ client/src/workflows/components/nodes/RequestNode.svelte
@@ -14,13 +14,11 @@
     Handle,
     type NodeProps,
     Position,
-    useNodeConnections,
     useSvelteFlow,
   } from "@xyflow/svelte";
   import NodeWrapper from "./NodeWrapper.svelte";
 
   const { updateNodeData, updateNode } = useSvelteFlow();
-  const connections = useNodeConnections({ handleType: "target" });
   const requestTypes = ["GET", "POST", "PUT"];
   const isGetRequestType = () => data.requestType === "GET";
   const getNodeWidth = () => (isGetRequestType() ? 200 : 320);
@@ -30,16 +28,15 @@
   let selectedRequestType = $state(data.requestType ?? "GET");
   let urlInputText = $state(data.url ?? "");
   let requestBodyInputText = $state(data.requestBody ?? "");
-  let inputIsConnectable = $derived(connections.current.length === 0);
   let initialWidth = $derived(getNodeWidth());
   let initialHeight = $derived(getNodeHeight());
 
   function isValidInputConnection(connection: Connection | EdgeBase) {
-    return connection.source !== id;
+    return connection.source !== id && connection.targetHandle !== "output";
   }
 
   function isValidOutputConnection(connection: Connection | EdgeBase) {
-    return connection.targetHandle === "input-flow-pin";
+    return connection.target !== id && connection.targetHandle === "input";
   }
 
   $effect(() => {
@@ -53,27 +50,24 @@
   minResizableWidth={initialWidth}
   minResizableHeight={initialHeight}
 >
-  <div class="space-y-2 w-full">
-    <div class="p-1">
-      <div class="relative">
-        <Handle
-          id="input-flow-pin"
-          type="target"
-          position={Position.Left}
-          isValidConnection={isValidInputConnection}
-          isConnectable={inputIsConnectable}
-        />
-        <Handle
-          id="output-flow-pin"
-          type="source"
-          position={Position.Right}
-          isValidConnection={isValidOutputConnection}
-        />
-      </div>
-    </div>
+  <div class="flex-col space-y-2 w-full">
+    <Handle
+      id="input"
+      type="target"
+      class="node-pin"
+      position={Position.Left}
+      isValidConnection={isValidInputConnection}
+    />
+    <Handle
+      id="output"
+      type="source"
+      class="node-pin"
+      position={Position.Right}
+      isValidConnection={isValidOutputConnection}
+    />
 
-    <fieldset class="">
-      <legend class="">Url</legend>
+    <fieldset>
+      <legend>Url</legend>
       <input
         class="nodrag w-full"
         placeholder="Enter a url..."
@@ -82,8 +76,8 @@
       />
     </fieldset>
 
-    <fieldset class="">
-      <legend class="">Type</legend>
+    <fieldset>
+      <legend>Type</legend>
       <select
         class="nodrag w-full"
         bind:value={selectedRequestType}
@@ -111,17 +105,5 @@
         </textarea>
       </fieldset>
     {/if}
-    <hr class="text-gray-100" />
-    <div class="p-1">
-      <div class="relative">
-        <p class="">Result value</p>
-        <Handle
-          id="result"
-          class="!bg-blue-300"
-          type="source"
-          position={Position.Right}
-        />
-      </div>
-    </div>
   </div>
 </NodeWrapper>
