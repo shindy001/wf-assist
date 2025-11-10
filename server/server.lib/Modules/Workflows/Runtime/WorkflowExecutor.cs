9efbc1865658204e37e1cab5dea304ef94b94f30
shindy001 <sindelar.p.etr@seznam.cz>
2025-11-10 15:09:54 +0100

    feat(server.lib): introduce WorkflowExecutor stub

---
+++ server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Logging;
+using WfAssist.AspNetCore.Modules.Workflows.Domain.Models;
+
+namespace WfAssist.AspNetCore.Modules.Workflows.Runtime;
+
+public sealed class WorkflowExecutor
+{
+    private readonly ILogger<WorkflowExecutor> _logger;
+
+    public WorkflowExecutor(ILogger<WorkflowExecutor> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// Execute specified <see cref="WorkflowSnapshot"/>.
+    /// </summary>
+    /// <param name="snapshot"><see cref="WorkflowSnapshot"/></param>
+    /// <returns></returns>
+    public Task Execute(WorkflowSnapshot snapshot)
+    {
+        _logger.LogInformation("Executing workflow '{workflowName}'.", snapshot.Name);
+        _logger.LogInformation("Workflow '{workflowName}' completed.", snapshot.Name);
+
+        return Task.CompletedTask;
+    }
+}
