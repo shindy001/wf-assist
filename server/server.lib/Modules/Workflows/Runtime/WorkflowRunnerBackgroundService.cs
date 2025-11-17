2e5f5ad7ee9113ea7394156e78635065f51a8966
shindy001 <sindelar.p.etr@seznam.cz>
2025-11-17 23:36:25 +0100

    feat(server.lib): extract WorkflowRunnerBackgroundService execute logic to ExecuteQueuedWorkflow method

--- server/server.lib/Modules/Workflows/Runtime/WorkflowRunnerBackgroundService.cs
+++ server/server.lib/Modules/Workflows/Runtime/WorkflowRunnerBackgroundService.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WfAssist.AspNetCore.Modules.Workflows.Domain.Models;
 using WfAssist.AspNetCore.Modules.Workflows.Infrastructure;
 
 namespace WfAssist.AspNetCore.Modules.Workflows.Runtime;
@@ -24,31 +25,44 @@ public class WorkflowRunnerBackgroundService : BackgroundService
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            try
-            {
-                await using var scope = _serviceScopeFactory.CreateAsyncScope();
-                var workflowProcessingRepository =
-                    scope.ServiceProvider.GetRequiredService<WorkflowProcessingRepository>();
-                var workflowExecutor = scope.ServiceProvider.GetRequiredService<WorkflowExecutor>();
-
-                var workflowRun = await workflowProcessingRepository.GetQueuedRun();
-                if (workflowRun is not null)
-                {
-                    _logger.LogInformation("Starting workflow run {runId}.", workflowRun.Id);
-                    await workflowExecutor.Execute(workflowRun.Snapshot);
-                    await workflowProcessingRepository.CompleteRun(workflowRun.Id);
-                    _logger.LogInformation("Workflow run {runId} completed.", workflowRun.Id);
-                }
-
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error while execution workflow run.");
-            }
+            await ExecuteQueuedWorkflow();
 
             await Task.Delay(_checkInterval, stoppingToken);
         }
 
         _logger.LogInformation("{service} is stopping.", nameof(WorkflowRunnerBackgroundService));
+    }
+
+    private async Task ExecuteQueuedWorkflow()
+    {
+        try
+        {
+            await using var scope = _serviceScopeFactory.CreateAsyncScope();
+            var workflowProcessingRepository = scope.ServiceProvider.GetRequiredService<WorkflowProcessingRepository>();
+            var workflowExecutor = scope.ServiceProvider.GetRequiredService<WorkflowExecutor>();
+            var workflowRun = await workflowProcessingRepository.GetQueuedRun();
+
+            if (workflowRun is not null)
+            {
+                await workflowProcessingRepository.UpdateRunStatus(workflowRun.Id, WorkflowRunStatus.Running);
+                _logger.LogInformation("Starting workflow run {runId}.", workflowRun.Id);
+
+                var success = await workflowExecutor.Execute(workflowRun.Snapshot);
+                if (success)
+                {
+                    await workflowProcessingRepository.UpdateRunStatus(workflowRun.Id, WorkflowRunStatus.Completed);
+                    _logger.LogInformation("Workflow run {runId} completed.", workflowRun.Id);
+                }
+                else
+                {
+                    await workflowProcessingRepository.UpdateRunStatus(workflowRun.Id, WorkflowRunStatus.Failed);
+                    _logger.LogInformation("Workflow run {runId} completed with errors.", workflowRun.Id);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unexpected error while execution workflow run.");
+        }
     }
 }
