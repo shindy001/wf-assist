92a129a2f90184560e0bfd8d5e7e9003eef9d39b
shindy001 <sindelar.p.etr@seznam.cz>
2025-11-17 23:32:50 +0100

    feat(server.lib): add try catch and return Execution state in WorkflowExecutor

--- server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
+++ server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
@@ -16,15 +16,23 @@ public sealed class WorkflowExecutor
     /// Execute specified <see cref="WorkflowSnapshot"/>.
     /// </summary>
     /// <param name="snapshot"><see cref="WorkflowSnapshot"/></param>
-    /// <returns></returns>
-    public Task Execute(WorkflowSnapshot snapshot)
+    /// <returns>True is execute was successful</returns>
+    // TODO - replace bool return with OneOf or Result object???
+    public Task<bool> Execute(WorkflowSnapshot snapshot)
     {
-        var executionOrder = WorkflowTopologySorter.CalculateNodeExecution(snapshot.Data);
+        try
+        {
+            var executionOrder = WorkflowTopologySorter.CalculateNodeExecution(snapshot.Data);
 
-        _logger.LogInformation("Executing workflow '{workflowName}'.", snapshot.Name);
-        _logger.LogInformation("Execution order: {executionOrder}", executionOrder.Select(x => x.Id));
-        _logger.LogInformation("Workflow '{workflowName}' completed.", snapshot.Name);
-
-        return Task.CompletedTask;
+            _logger.LogInformation("Executing workflow '{workflowName}'.", snapshot.Name);
+            _logger.LogInformation("Execution order: {executionOrder}", executionOrder.Select(x => x.Id));
+            _logger.LogInformation("Workflow '{workflowName}' completed.", snapshot.Name);
+            return Task.FromResult(true);
+        }
+        catch (Exception)
+        {
+            // TODO - return some error data with Result object
+            return Task.FromResult(false);
+        }
     }
 }
