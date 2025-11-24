e30ea36e9aadb76116601f9afd6ba8e61b28c420
shindy001 <sindelar.p.etr@seznam.cz>
2025-11-24 01:02:26 +0100

    feat(server.lib): implement execution logic in WorkflowExecutor

--- server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
+++ server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
@@ -1,14 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WfAssist.AspNetCore.Modules.Workflows.Domain.Models;
+using WfAssist.AspNetCore.Modules.Workflows.Runtime.NodeProcessors;
 
 namespace WfAssist.AspNetCore.Modules.Workflows.Runtime;
 
 public sealed class WorkflowExecutor
 {
+    private readonly IServiceProvider _serviceProvider;
     private readonly ILogger<WorkflowExecutor> _logger;
 
-    public WorkflowExecutor(ILogger<WorkflowExecutor> logger)
+    public WorkflowExecutor(IServiceProvider serviceProvider, ILogger<WorkflowExecutor> logger)
     {
+        _serviceProvider = serviceProvider;
         _logger = logger;
     }
 
@@ -22,10 +26,30 @@ public sealed class WorkflowExecutor
     {
         try
         {
-            var executionOrder = WorkflowTopologySorter.CalculateNodeExecution(snapshot.Data);
-
             _logger.LogInformation("Executing workflow '{workflowName}'.", snapshot.Name);
+
+            var executionOrder = WorkflowTopologySorter.CalculateNodeExecution(snapshot.Data);
             _logger.LogInformation("Execution order: {executionOrder}", executionOrder.Select(x => x.Id));
+
+            foreach (var node in executionOrder)
+            {
+                var dataType = node.Data.GetType();
+                var processor = _serviceProvider.GetKeyedService<IWorkflowNodeProcessor>(nameof(RequestNodeData));
+
+                if (processor is null)
+                {
+                    _logger.LogError(
+                        """
+                        Processor for node data type {dataTypeName} was not found, ensure that processor is registered.
+                        Aborting workflow run.
+                        """, dataType.Name);
+
+                    return Task.FromResult(false);
+                }
+
+                processor.Process(node);
+            }
+
             _logger.LogInformation("Workflow '{workflowName}' completed.", snapshot.Name);
             return Task.FromResult(true);
         }
