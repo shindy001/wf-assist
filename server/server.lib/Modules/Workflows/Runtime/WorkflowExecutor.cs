4143a952f91501da915d275db272ef7501c6317e
shindy001 <sindelar.p.etr@seznam.cz>
2025-12-01 00:50:15 +0100

    refactor(server.lib): simplify runner and executor to only handle exception in runner, also save results in runner in case of error

--- server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
+++ server/server.lib/Modules/Workflows/Runtime/WorkflowExecutor.cs
@@ -1,17 +1,19 @@
 using System.Collections.Immutable;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OneOf.Types;
 using WfAssist.AspNetCore.Modules.Workflows.Domain.Models;
 using WfAssist.AspNetCore.Modules.Workflows.Runtime.NodeProcessors;
 
 namespace WfAssist.AspNetCore.Modules.Workflows.Runtime;
 
-public sealed partial class WorkflowExecutor
+internal sealed partial class WorkflowExecutor
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly ILogger<WorkflowExecutor> _logger;
 
-    public WorkflowExecutor(IServiceProvider serviceProvider, ILogger<WorkflowExecutor> logger)
+    public WorkflowExecutor(IServiceProvider serviceProvider,
+        ILogger<WorkflowExecutor> logger)
     {
         _serviceProvider = serviceProvider;
         _logger = logger;
@@ -21,48 +23,35 @@ public sealed partial class WorkflowExecutor
     /// Execute specified <see cref="WorkflowSnapshot"/>.
     /// </summary>
     /// <param name="snapshot"><see cref="WorkflowSnapshot"/></param>
-    /// <returns>Bool to specify if run was successful and results of the run.</returns>
-    public async Task<(bool success, ImmutableArray<ProcessingResult> results)> Execute(WorkflowSnapshot snapshot)
+    public async Task Execute(WorkflowSnapshot snapshot)
     {
-        var processResults = new List<ProcessingResult>();
+        LogExecutionStart(snapshot.Name);
 
-        try
+        var executionOrder = WorkflowTopologySorter.CalculateNodeExecution(snapshot.Data);
+        LogExecutionOrder(executionOrder.Select(x => x.Id));
+
+        foreach (var node in executionOrder)
         {
-            LogExecutionStart(snapshot.Name);
+            var dataType = node.Data.GetType();
+            var processor = _serviceProvider.GetKeyedService<IWorkflowNodeProcessor>(nameof(RequestNodeData));
 
-            var executionOrder = WorkflowTopologySorter.CalculateNodeExecution(snapshot.Data);
-            LogExecutionOrder(executionOrder.Select(x => x.Id));
-
-            foreach (var node in executionOrder)
+            if (processor is null)
             {
-                var dataType = node.Data.GetType();
-                var processor = _serviceProvider.GetKeyedService<IWorkflowNodeProcessor>(nameof(RequestNodeData));
-
-                if (processor is null)
-                {
-                    LogProcessorNotFoundError(dataType.Name);
-
-                    return (false, [..processResults]);
-                }
-
-                var result = await processor.Process(node);
-                processResults.Add(result);
+                LogExecutionInterrupted(snapshot.Name,
+                    $"Processor for node data type {dataType.Name} was not found, ensure that processor is registered.");
+                return;
             }
 
-            LogExecutionCompleted(snapshot.Name);
+            var result = await processor.Process(node);
 
-            var successful = processResults.All(x => x.Successful);
-            return (successful, [..processResults]);
+            if (result.Value is Error)
+            {
+                LogExecutionInterrupted(snapshot.Name, $"Error during processing of node {node.Id}.");
+                return;
+            }
         }
-        catch (Exception e)
-        {
-            LogUnexpectedErrorDuringRun($"{snapshot.Id}_{snapshot.Name}", e.Message);
 
-            processResults.Add(ProcessingResult.Error(
-                $"Unexpected error during workflow {snapshot.Id}_{snapshot.Name} run: {e.Message}", string.Empty));
-
-            return (false, [..processResults]);
-        }
+        LogExecutionCompleted(snapshot.Name);
     }
 
     [LoggerMessage(LogLevel.Information, "Executing workflow '{workflowName}'.")]
@@ -71,16 +60,10 @@ public sealed partial class WorkflowExecutor
     [LoggerMessage(LogLevel.Information, "Execution order: {executionOrder}")]
     partial void LogExecutionOrder(IEnumerable<string> executionOrder);
 
-    [LoggerMessage(LogLevel.Error, """
-                                   Processor for node data type {dataTypeName} was not found, ensure that processor is registered.
-                                   Aborting workflow run.
-                                   """)]
-    partial void LogProcessorNotFoundError(string dataTypeName);
-
-    [LoggerMessage(LogLevel.Error, "Unexpected error during workflow {workflow} run: {errorMessage}")]
-    partial void LogUnexpectedErrorDuringRun(string workflow, string errorMessage);
-
     [LoggerMessage(LogLevel.Information, "Workflow '{workflowName}' completed.")]
     partial void LogExecutionCompleted(string workflowName);
+
+    [LoggerMessage(LogLevel.Error, "Workflow '{workflowName}' execution was interrupted. Reason: {reason}.")]
+    partial void LogExecutionInterrupted(string workflowName, string reason);
 
 }
--- server/server.lib/Modules/Workflows/Runtime/WorkflowRunnerBackgroundService.cs
+++ server/server.lib/Modules/Workflows/Runtime/WorkflowRunnerBackgroundService.cs
@@ -6,7 +6,7 @@ using WfAssist.AspNetCore.Modules.Workflows.Infrastructure;
 
 namespace WfAssist.AspNetCore.Modules.Workflows.Runtime;
 
-public class WorkflowRunnerBackgroundService : BackgroundService
+internal sealed class WorkflowRunnerBackgroundService : BackgroundService
 {
     private readonly IServiceScopeFactory _serviceScopeFactory;
     private readonly ILogger<WorkflowRunnerBackgroundService> _logger;
@@ -35,36 +35,48 @@ public class WorkflowRunnerBackgroundService : BackgroundService
 
     private async Task ExecuteQueuedWorkflow()
     {
+        await using var scope = _serviceScopeFactory.CreateAsyncScope();
+        var workflowProcessingRepository = scope.ServiceProvider.GetRequiredService<WorkflowProcessingRepository>();
+        var workflowExecutor = scope.ServiceProvider.GetRequiredService<WorkflowExecutor>();
+        var processingContext = scope.ServiceProvider.GetRequiredService<ProcessingContext>();
+        WorkflowRun? workflowRun = null;
+
         try
         {
-            await using var scope = _serviceScopeFactory.CreateAsyncScope();
-            var workflowProcessingRepository = scope.ServiceProvider.GetRequiredService<WorkflowProcessingRepository>();
-            var workflowExecutor = scope.ServiceProvider.GetRequiredService<WorkflowExecutor>();
-            var workflowRun = await workflowProcessingRepository.GetQueuedRun();
+            workflowRun = await workflowProcessingRepository.GetQueuedRun();
 
             if (workflowRun is not null)
             {
                 await workflowProcessingRepository.UpdateRunStatus(workflowRun.Id, WorkflowRunStatus.Running);
                 _logger.LogInformation("Starting workflow run {runId}.", workflowRun.Id);
 
-                var (success, results) = await workflowExecutor.Execute(workflowRun.Snapshot);
-                if (success)
+                await workflowExecutor.Execute(workflowRun.Snapshot);
+                if (processingContext.IsProcessingSuccessful())
                 {
                     await workflowProcessingRepository.CompleteRun(workflowRun.Id, WorkflowRunStatus.Completed,
-                        results.ToList());
+                        processingContext.ProcessingResults.Values.ToList());
                     _logger.LogInformation("Workflow run {runId} completed.", workflowRun.Id);
                 }
                 else
                 {
                     await workflowProcessingRepository.CompleteRun(workflowRun.Id, WorkflowRunStatus.Failed,
-                        results.ToList());
+                        processingContext.ProcessingResults.Values.ToList());
                     _logger.LogInformation("Workflow run {runId} completed with errors.", workflowRun.Id);
                 }
             }
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            _logger.LogError(ex, "Unexpected error while execution workflow run.");
+            _logger.LogError(e, "Unexpected error while execution workflow run.");
+
+            if (workflowRun is not null)
+            {
+                processingContext.AddResult("ProcessingError", ProcessingResult.Error(
+                    $"Unexpected error during workflow run: {e.Message}", string.Empty));
+
+                await workflowProcessingRepository.CompleteRun(workflowRun.Id, WorkflowRunStatus.Failed,
+                    processingContext.ProcessingResults.Values.ToList());
+            }
         }
     }
 }
