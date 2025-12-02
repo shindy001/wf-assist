14f2c440d04e397697cf29609972efca238105fd
shindy001 <sindelar.p.etr@seznam.cz>
2025-12-02 11:51:16 +0100

    feat(server.lib): use WorkflowNodeReferenceResolver to resolve references in request node data before processing

--- server/server.lib/Modules/Workflows/Runtime/NodeProcessors/RequestWorkflowNodeProcessor.cs
+++ server/server.lib/Modules/Workflows/Runtime/NodeProcessors/RequestWorkflowNodeProcessor.cs
@@ -1,6 +1,8 @@
 using System.Net.Mime;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using OneOf;
 using OneOf.Types;
 using WfAssist.AspNetCore.Modules.Workflows.Domain.Models;
@@ -13,12 +15,15 @@ internal sealed class RequestWorkflowNodeProcessor : IWorkflowNodeProcessor
 
     private readonly HttpClient _httpClient;
     private readonly ProcessingContext _processingContext;
+    private readonly WorkflowNodeReferenceResolver _nodeReferenceResolver;
 
     public RequestWorkflowNodeProcessor([FromKeyedServices(HttpClientKey)] HttpClient httpClient,
-        ProcessingContext processingContext)
+        ProcessingContext processingContext,
+        WorkflowNodeReferenceResolver nodeReferenceResolver)
     {
         _httpClient = httpClient;
         _processingContext = processingContext;
+        _nodeReferenceResolver = nodeReferenceResolver;
     }
 
     public async Task<OneOf<Success, Error>> Process(WorkflowNode workflowNode)
@@ -28,8 +33,19 @@ internal sealed class RequestWorkflowNodeProcessor : IWorkflowNodeProcessor
             throw new ArgumentException($"Expected node data type {nameof(RequestNodeData)} but got {workflowNode.Data.GetType()}");
         }
 
-        var requestMessage = GetRequestMessage(requestNodeData.RequestType, requestNodeData.Url, requestNodeData.RequestBody);
+        var data = ResolveNodeReferences(requestNodeData);
+        var requestMessage = GetRequestMessage(data.RequestType, data.Url, data.RequestBody);
+
         var response = await _httpClient.SendAsync(requestMessage);
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _processingContext.AddResult(workflowNode.Id, ProcessingResult.Error(
+                $"Status code: {response.StatusCode}, reason: {response.ReasonPhrase}",
+                responseBody));
+            return new Error();
+        }
 
         if (response.Content.Headers.ContentType?.MediaType != MediaTypeNames.Application.Json)
         {
@@ -40,19 +56,19 @@ internal sealed class RequestWorkflowNodeProcessor : IWorkflowNodeProcessor
             return new Error();
         }
 
-        var responseBody = await response.Content.ReadAsStringAsync();
-        if (!response.IsSuccessStatusCode)
-        {
-            _processingContext.AddResult(workflowNode.Id, ProcessingResult.Error(
-                $"Status code: {response.StatusCode}, reason: {response.ReasonPhrase}",
-                responseBody));
-            return new Error();
-        }
-
         var (resultValueType, resultData) = ParseResponse(responseBody.Trim());
         _processingContext.AddResult(workflowNode.Id,
             ProcessingResult.Success(workflowNode.Id, resultValueType, resultData));
         return new Success();
+    }
+
+    private RequestNodeData ResolveNodeReferences(RequestNodeData requestNodeData)
+    {
+        return requestNodeData with
+        {
+            Url = _nodeReferenceResolver.Resolve(requestNodeData.Url),
+            RequestBody = _nodeReferenceResolver.Resolve(requestNodeData.RequestBody ?? string.Empty)
+        };
     }
 
     private static (ProcessResultValueType resultValueType, object? resultData) ParseResponse(string responseBody)
@@ -68,12 +84,13 @@ internal sealed class RequestWorkflowNodeProcessor : IWorkflowNodeProcessor
 
     private static HttpRequestMessage GetRequestMessage(RequestType requestType, string url, string? body)
     {
+        var content = new StringContent(body ?? string.Empty, Encoding.UTF8, MediaTypeNames.Application.Json);
         return requestType switch {
             RequestType.Get => new HttpRequestMessage(HttpMethod.Get, url),
-            RequestType.Post => new HttpRequestMessage(HttpMethod.Get, url) { Content = new StringContent(body ?? string.Empty) },
-            RequestType.Put => new HttpRequestMessage(HttpMethod.Get, url) { Content = new StringContent(body ?? string.Empty) },
-            RequestType.Patch => new HttpRequestMessage(HttpMethod.Get, url) { Content = new StringContent(body ?? string.Empty) },
-            RequestType.Delete => new HttpRequestMessage(HttpMethod.Get, url) { Content = new StringContent(body ?? string.Empty) },
+            RequestType.Post => new HttpRequestMessage(HttpMethod.Post, url) { Content = content },
+            RequestType.Put => new HttpRequestMessage(HttpMethod.Put, url) { Content = content },
+            RequestType.Patch => new HttpRequestMessage(HttpMethod.Patch, url) { Content = content },
+            RequestType.Delete => new HttpRequestMessage(HttpMethod.Delete, url) { Content = content },
             _ => throw new InvalidOperationException($"Unsupported request type {requestType}")
         };
     }
