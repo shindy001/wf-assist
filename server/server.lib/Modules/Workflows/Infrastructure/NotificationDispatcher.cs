b7d2677fe21aef2f4d39387db80c1dea06de66e7
shindy001 <sindelar.p.etr@seznam.cz>
2025-12-10 00:14:29 +0100

    feat(server.lib): close all channels during app shutdown to not block the shutdown

--- server/server.lib/Modules/Workflows/Infrastructure/NotificationDispatcher.cs
+++ server/server.lib/Modules/Workflows/Infrastructure/NotificationDispatcher.cs
@@ -1,5 +1,7 @@
 using System.Collections.Concurrent;
 using System.Threading.Channels;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WfAssist.AspNetCore.Modules.Workflows.Domain.Models.Notifications;
 
 namespace WfAssist.AspNetCore.Modules.Workflows.Infrastructure;
@@ -7,6 +9,11 @@ namespace WfAssist.AspNetCore.Modules.Workflows.Infrastructure;
 internal sealed class NotificationDispatcher
 {
     private readonly ConcurrentDictionary<Guid, Channel<Notification>> _channels = new();
+
+    public NotificationDispatcher(IHostApplicationLifetime applicationLifetime, ILogger<NotificationDispatcher> logger)
+    {
+        RegisterCloseChannelsOnAppShutdownCallback(applicationLifetime, logger);
+    }
 
     public ChannelReader<Notification> Register(Guid clientId)
     {
@@ -29,6 +36,18 @@ internal sealed class NotificationDispatcher
         {
             await channel.Value.Writer.WriteAsync(notification);
         }
+    }
+
+    private void RegisterCloseChannelsOnAppShutdownCallback(IHostApplicationLifetime applicationLifetime, ILogger<NotificationDispatcher> logger)
+    {
+        applicationLifetime.ApplicationStopping.Register(() =>
+        {
+            logger.LogInformation($"{nameof(NotificationDispatcher)}: Application is stopping, closing channels...");
+            foreach (var channel in _channels)
+            {
+                channel.Value.Writer.TryComplete();
+            }
+        });
     }
 }
 
