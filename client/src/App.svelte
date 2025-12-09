7d19a0331cf6bfa15fe4a0ffc302c2ecf977e029
shindy001 <sindelar.p.etr@seznam.cz>
2025-12-09 01:08:11 +0100

    feat(client): register global Toaster component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,6 +4,7 @@
   import { Icons } from "$lib/components/ui/icons";
   import { ModeWatcher } from "mode-watcher";
   import { TurboEdgeGradient } from "$lib/css";
+  import { Toaster } from "svelte-sonner";
 </script>
 
 <div class="size-full">
@@ -11,4 +12,5 @@
   <Icons />
   <TurboEdgeGradient />
   <Workflows />
+  <Toaster theme="dark" position="top-right" duration={5000} expand={true} />
 </div>
