d0a06c1bd9bc2f570f68dcae8051241a9d616efb
shindy001 <sindelar.p.etr@seznam.cz>
2025-12-09 01:04:57 +0100

    fix(client): force correct api client baseUrl by creating runtime config

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -1,4 +1,4 @@
-﻿import { defineConfig } from "@hey-api/openapi-ts";
+import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   input: "./../server/server.host/wfAssist-api.json",
@@ -6,4 +6,10 @@ export default defineConfig({
     path: "src/api",
     lint: null,
   },
+  plugins: [
+    {
+      name: "@hey-api/client-fetch",
+      runtimeConfigPath: "./../apiClientConfig.ts",
+    },
+  ],
 });
