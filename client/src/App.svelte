89f1abfcaa045a0dbee6276dd536da0a1cf384c0
shindy001 <sindelar.p.etr@seznam.cz>
2026-01-25 22:54:37 +0100

    refactor(client): remove mode-watcher package as its not needed right now

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,13 +2,11 @@
   import "./app.css";
   import Workflows from "./workflows/Workflows.svelte";
   import { Icons } from "$lib/components/ui/icons";
-  import { ModeWatcher } from "mode-watcher";
   import { TurboEdgeGradient } from "$lib/css";
   import { Toaster } from "$lib/components/ui/toaster";
 </script>
 
 <div class="size-full">
-  <ModeWatcher track={false} defaultMode={"dark"} />
   <Icons />
   <TurboEdgeGradient />
   <Workflows />
