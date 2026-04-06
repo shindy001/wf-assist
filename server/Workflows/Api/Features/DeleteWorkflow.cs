239c3e7a49b687e373fab1dc1dbc2d1dcdd69590
shindy001 <sindelar.p.etr@seznam.cz>
2026-04-07 00:27:37 +0200

    refactor(server): extract DeleteWorkflow command logic to local DeleteWorkflowCommandHandler

--- server/Workflows/Api/Features/DeleteWorkflow.cs
+++ server/Workflows/Api/Features/DeleteWorkflow.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using OneOf.Types;
+using Shared.CQRS;
 using WfAssist.Workflows.Core.Services;
 
 namespace WfAssist.Workflows.Api.Features;
@@ -9,12 +11,24 @@ public static class Delete
 {
     public static void MapDeleteWorkflowEndpoint(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapDelete("/{id:guid}", async (Guid id, IWorkflowRepository workflowRepository) =>
+        endpoints.MapDelete("/{id:guid}", async (Guid id, ICommandDispatcher commandDispatcher) =>
             {
-                await workflowRepository.Delete(id);
+                await commandDispatcher.Dispatch(new DeleteWorkflowCommand(id));
 
                 return TypedResults.NoContent();
             })
             .Produces(StatusCodes.Status204NoContent);
+    }
+}
+
+internal record DeleteWorkflowCommand(Guid Id) : ICommand<Success>;
+
+internal sealed class DeleteWorkflowCommandHandler(IWorkflowRepository workflowRepository)
+    : ICommandHandler<DeleteWorkflowCommand, Success>
+{
+    public async Task<Success> Handle(DeleteWorkflowCommand command, CancellationToken cancellationToken = default)
+    {
+        await workflowRepository.Delete(command.Id);
+        return new Success();
     }
 }
