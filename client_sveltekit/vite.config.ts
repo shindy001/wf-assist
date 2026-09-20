7921e5cbc83c1efc556ebda72b5fabee25e4084e
shindy001 <sindelar.p.etr@seznam.cz>
2026-09-20 16:07:20 +0200

    feat(client): create sveltekit project as preparation for svelte spa client to sveltekit static app migration

---
+++ client_sveltekit/vite.config.ts
@@ -0,0 +1,35 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vitest/config';
+import adapter from '@sveltejs/adapter-static';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+				experimental: { async: true }
+			},
+
+			adapter: adapter({
+				fallback: '200.html' // may differ from host to host
+			})
+		})
+	],
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'unit',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
+});
