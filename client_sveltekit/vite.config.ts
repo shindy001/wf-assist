41ab2dfddc50fb810d7a68d88a566a92a06fb19f
shindy001 <sindelar.p.etr@seznam.cz>
2026-09-20 23:08:57 +0200

    chore(client): format sveltekit files

--- client_sveltekit/vite.config.ts
+++ client_sveltekit/vite.config.ts
@@ -1,40 +1,41 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vitest/config';
-import adapter from '@sveltejs/adapter-static';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vitest/config";
+import adapter from "@sveltejs/adapter-static";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			preprocess: vitePreprocess(),
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
-				experimental: { async: true }
-			},
+  plugins: [
+    tailwindcss(),
+    sveltekit({
+      preprocess: vitePreprocess(),
+      compilerOptions: {
+        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+        runes: ({ filename }) =>
+          filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+        experimental: { async: true },
+      },
 
-			adapter: adapter({
-				fallback: '200.html' // may differ from host to host
-			}),
-			alias: {
-				$app: '/node_modules/@sveltejs/kit/assets/app',
-			}
-		})
-	],
-	test: {
-		expect: { requireAssertions: true },
-		projects: [
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'unit',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+      adapter: adapter({
+        fallback: "200.html", // may differ from host to host
+      }),
+      alias: {
+        $app: "/node_modules/@sveltejs/kit/assets/app",
+      },
+    }),
+  ],
+  test: {
+    expect: { requireAssertions: true },
+    projects: [
+      {
+        extends: "./vite.config.ts",
+        test: {
+          name: "unit",
+          environment: "node",
+          include: ["src/**/*.{test,spec}.{js,ts}"],
+          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+        },
+      },
+    ],
+  },
 });
